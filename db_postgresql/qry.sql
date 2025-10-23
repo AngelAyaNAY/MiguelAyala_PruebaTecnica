a1d69f8d63fbba169d4c9fc2f2e2660f04d46053
MiguelAyala <ggg.gg@g.com>
2025-10-22 23:53:39 -0500

    se realizan ajustes hacia el front

--- db_postgresql/qry.sql
+++ db_postgresql/qry.sql
@@ -11,4 +11,16 @@ CREATE TABLE usuarios (
 INSERT INTO usuarios (nombre, correo, edad)
 	VALUES  ('Miguel Ayala', 'angel.aya.pin.00@gmail.com', 25);
 
+INSERT INTO usuarios (nombre, correo, edad)
+	VALUES  ('Laura Ramírez', 'laura.ramirez23@gmail.com', 28),
+	  		('Andrés Torres', 'andres.torres.dev@gmail.com', 31),
+	  		('Camila Rojas', 'camila.rojas.art@gmail.com', 24),
+	  		('Juan Esteban', 'juanesteban.codes@gmail.com', 27),
+	  		('Valentina Gómez', 'valentina.gomez01@gmail.com', 22),
+	  		('David Hernández', 'davidhdez92@gmail.com', 33),
+	  		('Sara Castillo', 'saracastillo.design@gmail.com', 26),
+	  		('Carlos Méndez', 'carlos.mendez.tech@gmail.com', 29),
+	  		('Natalia López', 'natalia.lopez.photo@gmail.com', 30);
+
+
 SELECT * FROM usuarios;
